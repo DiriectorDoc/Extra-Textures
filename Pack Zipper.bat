4ee335789f6df39583575ce2103c33167f42d9c9
Diriector_Doc <davijust213@yahoo.ca>
2021-06-04 15:06:13 -0400

    Update Pack Zipper.bat

--- Pack Zipper.bat
+++ Pack Zipper.bat
@@ -4,6 +4,7 @@ set name=pack
 for /f "tokens=* delims=" %%f in (zipconfig.txt) do (
 	set %%f
 )
+del "%name%.zip"
 "c:\Program Files\WinRAR\WinRAR.exe" a -afzip "%name%.zip" assets
 "c:\Program Files\WinRAR\WinRAR.exe" a -afzip "%name%.zip" pack.png
 "c:\Program Files\WinRAR\WinRAR.exe" a -afzip "%name%.zip" pack.mcmeta
